ef77394bf8f5b886b0ba23daec602be67d97321a
downfall318 <140861164+downfall318@users.noreply.github.com>
2026-09-02 17:02:42 -0500

    Track localized steel dents independently of item health

--- Scripts/4_World/ArPen/ArPenArmorState.c
+++ Scripts/4_World/ArPen/ArPenArmorState.c
@@ -1,8 +1,15 @@
+class ArPenDentSite
+{
+    vector ModelPosition;
+    float DepthMM;
+};
+
 modded class ItemBase
 {
     protected bool m_ArPenKruppInitialized;
     protected float m_ArPenCurrentKrupp;
     protected float m_ArPenCurrentArmorHealth;
+    protected ref array<ref ArPenDentSite> m_ArPenDentSites;
 
     float ArPen_GetCurrentKrupp(ArPenArmorData armorData)
     {
@@ -19,6 +26,44 @@ modded class ItemBase
     {
         ArPen_GetCurrentKrupp(armorData);
         return m_ArPenCurrentArmorHealth;
+    }
+
+    float ArPen_GetLocalDentDepth(vector modelPosition, float radiusMM)
+    {
+        if (!m_ArPenDentSites)
+            return 0.0;
+
+        float radiusM = Math.Max(radiusMM, 0.0) * 0.001;
+        float depthMM;
+        foreach (ArPenDentSite site : m_ArPenDentSites)
+        {
+            if (site && vector.Distance(site.ModelPosition, modelPosition) <= radiusM)
+                depthMM = Math.Max(depthMM, site.DepthMM);
+        }
+        return depthMM;
+    }
+
+    void ArPen_RecordDent(vector modelPosition, float radiusMM, float addedDepthMM)
+    {
+        if (addedDepthMM <= 0.0)
+            return;
+        if (!m_ArPenDentSites)
+            m_ArPenDentSites = new array<ref ArPenDentSite>;
+
+        float radiusM = Math.Max(radiusMM, 0.0) * 0.001;
+        foreach (ArPenDentSite site : m_ArPenDentSites)
+        {
+            if (site && vector.Distance(site.ModelPosition, modelPosition) <= radiusM)
+            {
+                site.DepthMM = site.DepthMM + addedDepthMM;
+                return;
+            }
+        }
+
+        ArPenDentSite newSite = new ArPenDentSite();
+        newSite.ModelPosition = modelPosition;
+        newSite.DepthMM = addedDepthMM;
+        m_ArPenDentSites.Insert(newSite);
     }
 
     void ArPen_AbsorbDamage(ArPenArmorData armorData, float absorbedDamage)
